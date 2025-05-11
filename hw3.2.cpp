38268a08ad56e5a1e00fe222754ec3eecb89e191
E1B59 <0602joanna@gmail.com>
2025-05-11 14:26:54 +0800

    撰寫主程式a選項

---
+++ hw3.2.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>//random number
+#include <conio.h>
+#include <unistd.h>//delay
+
+void WelcomeScreen() {
+    for (int i = 0; i < 20; i++) {
+        printf("* Welcome *\n");
+    }
+}
+void List(){//Print Function List 
+	printf("----------[Booking System]----------\n");
+	printf("|       A. Available seats          |\n");
+	printf("|       B. Arrange for you          |\n");
+	printf("|      C. Choose by yourself        |\n");
+	printf("|           D. Exit                 |\n");
+	printf("------------------------------------\n\n");
+	printf("Please enter options: \n");
+} 
+int main(void) {
+	int Row,Column,KeyCount=2,FunctionKeyIn=0,Key,RandomSeatCount;
+	int i,j,k;
+	char Seats[9][9];
+	
+	for(Row=0;Row<9;Row++){//Seats Reset
+		for(Column=0;Column<9;Column++){
+			Seats[Row][Column]='-';
+		}
+	}
+	
+	srand(time(NULL));
+	RandomSeatCount=0;
+	
+	while(RandomSeatCount<10){//Set Random Seat
+		Row = rand()%9;
+		Column = rand()%9;
+		if(Seats[Row][Column]=='-'){
+			Seats[Row][Column]='*';
+			RandomSeatCount++; 
+		}
+	}
+	
+	WelcomeScreen();
+	
+	do{//Key In 
+		printf("Please enter your password, %d chances remaining (password:2025): ",KeyCount);
+		scanf("%d",&Key);
+		printf("\n");
+		KeyCount--;
+		if(KeyCount==-1){
+			system("cls");
+			return 0;
+		}
+	}while(Key!=2025);
+	
+	system("cls");
+	printf("----------Welcome E1B59----------");
+	system("cls");
+	
+	while(1){
+		List();
+		FunctionKeyIn=getch();
+		system("cls");
+		while(FunctionKeyIn!=65 &&FunctionKeyIn!=66 &&FunctionKeyIn!=67 &&FunctionKeyIn!=68 &&FunctionKeyIn!=97 &&FunctionKeyIn!=98 &&FunctionKeyIn!=99 &&FunctionKeyIn!=100){
+			List();
+			printf("\nError message.Please try again\n");
+			FunctionKeyIn=getch();
+			system("cls");
+		}
+		
+		if(FunctionKeyIn==65 || FunctionKeyIn==97){//Function A
+			printf("This is the current seat : \n\n");
+			printf(" 123456789\n");
+			for(i=8;i>=0;i--){
+				printf("%d",i+1);
+				for(j=0;j<9;j++){
+					printf("%c",Seats[i][j]);
+				}
+				printf("\n");
+			}
+			printf("Press any key to continue.\n");
+			getch();
+			system("cls");	
+		}
+		
+		
+				
+		}
+	}	
+
+
